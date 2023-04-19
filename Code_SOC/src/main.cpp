8172464996d22453f81ca1275cbdde57c6fd3ddb
estebanolroberto <robertoestebanollivares@gmail.com>
2023-04-19 19:11:55 +0200

    Se añade una función para el codigo y se añaden más variables al codigo

--- Code_SOC/src/main.cpp
+++ Code_SOC/src/main.cpp
@@ -9,26 +9,27 @@
 #include "MQTT.hpp"
 #include "ESP32_Utils.hpp"
 #include "ESP32_Utils_MQTT_Async.hpp"
+
+
+#define BMP280_ADDRESS (0x76)
+#define HTU21DF_I2CADDR (0x40)
 const char *HTU_MQTT_TOPIC = "sensorHTU";
 const char *BMP_MQTT_TOPIC = "sensorBMP";          
 Adafruit_HTU21DF htu21d = Adafruit_HTU21DF();
 Adafruit_BMP280 bmp280;
 HTTPClient http;
+hw_timer_t * timer = NULL;
 
-unsigned long lastRead = 0;
 bool htu21dDetected = false;
 bool bmp280Detected = false;
-bool noSensorDetected = false;
+volatile bool interruptFlag = false;
+
 String URL = url;
 String lastItem = "";
 String currentItem = "";
-
-String getLastItem();
-void  readSensors();
-volatile bool interruptFlag = false;
-int totalInterruptCounter;
+int HTTPCODE_SUCCESS = 200;
+long time_presenceI2C= 5000000;
  
-hw_timer_t * timer = NULL;
  
 void IRAM_ATTR onTimer() {
   interruptFlag = true;
@@ -44,76 +45,76 @@ void setup() {
   http.begin(URL);
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 5000000, true);
+  timerAlarmWrite(timer, time_presenceI2C, true);
   timerAlarmEnable(timer);
 }
 
 void loop() {
 
+  if (interruptFlag) {
+    interruptFlag = false;
+    handleSensorData();
+  }
+}
 
+void handleSensorData() {
   unsigned long currentMillis = millis();
+
   StaticJsonDocument<200> sensor_htu;
   StaticJsonDocument<200> sensor_bmp;
   String String_sensor_htu;
   String String_sensor_bmp;
 
+  currentItem = getLastItem();
+  if (currentItem != lastItem) {
+    Serial.println("Nuevo elemento añadido: " + currentItem);
+    lastItem = currentItem;
+  }
 
-  if (interruptFlag) {
-    interruptFlag = false;
-    currentItem = getLastItem();
-    if (currentItem != lastItem) {
-      Serial.println("Nuevo elemento añadido: " + currentItem);
-      lastItem = currentItem;
-    }
+  Wire.beginTransmission(BMP280_ADDRESS);
+  if (Wire.endTransmission() == 0) {
+    bmp280.begin();
+    Serial.println("Se detectó el sensor BMP280.");
+    Serial.print("Temperatura: ");
+    Serial.print(bmp280.readTemperature());
+    sensor_bmp["temperature"] = bmp280.readTemperature();
+    Serial.println(" *C");
+    Serial.print("Presión: ");
+    Serial.print(bmp280.readPressure() * 0.01);
+    sensor_bmp["pressure"] = bmp280.readPressure();
+    Serial.println(" mbar");
+    Serial.print("Altitud: ");
+    Serial.print(bmp280.readAltitude());
+    sensor_bmp["altitude"] = bmp280.readAltitude();
+    Serial.println(" m");
+    Serial.println();
+    serializeJson(sensor_bmp, String_sensor_bmp);
+    PublishMqtt(String_sensor_bmp.c_str(), BMP_MQTT_TOPIC);
+    bmp280Detected = true;
+  }
 
-    Wire.beginTransmission(0x76);
-    if (Wire.endTransmission() == 0) {
-      bmp280.begin(0x76);
-      Serial.println("Se detectó el sensor BMP280.");
-      Serial.print("Temperatura: ");
-      Serial.print(bmp280.readTemperature());
-      sensor_bmp["temperature"] = bmp280.readTemperature();
-      Serial.println(" *C");
-      Serial.print("Presión: ");
-      Serial.print(bmp280.readPressure()*0.01);
-      sensor_bmp["pressure"] = bmp280.readPressure();
-      Serial.println(" mbar");
-      Serial.print("Altitud: ");
-      Serial.print(bmp280.readAltitude());
-      sensor_bmp["altitude"] = bmp280.readAltitude();
-      Serial.println(" m");
-      Serial.println();
-      serializeJson(sensor_bmp, String_sensor_bmp);
-      PublishMqtt(String_sensor_bmp.c_str(), BMP_MQTT_TOPIC);
-      bmp280Detected = true;
-    }
+  Wire.beginTransmission(HTU21DF_I2CADDR);
+  if (Wire.endTransmission() == 0) {
+    htu21d.begin();
+    Serial.println("Se detectó el sensor HTU21D.");
+    Serial.print("Humedad: ");
+    Serial.print(htu21d.readHumidity());
+    sensor_htu["humidity"] = htu21d.readHumidity();
+    Serial.println(" %");
+    Serial.print("Temperatura: ");
+    Serial.print(htu21d.readTemperature());
+    sensor_htu["temperature"] = htu21d.readTemperature();
+    Serial.println(" *C");
+    Serial.println();
+    serializeJson(sensor_htu, String_sensor_htu);
+    PublishMqtt(String_sensor_htu.c_str(), HTU_MQTT_TOPIC);
+    htu21dDetected = true;
+  }
 
-    Wire.beginTransmission(0x40);
-    if (Wire.endTransmission() == 0) {
-      htu21d.begin();
-      Serial.println("Se detectó el sensor HTU21D.");
-      Serial.print("Humedad: ");
-      Serial.print(htu21d.readHumidity());
-      sensor_htu["humidity"] = htu21d.readHumidity();
-      Serial.println(" %");
-      Serial.print("Temperatura: ");
-      Serial.print(htu21d.readTemperature());
-      sensor_htu["temperature"] = htu21d.readTemperature();
-      Serial.println(" *C");
-      Serial.println();
-      serializeJson(sensor_htu, String_sensor_htu);
-      PublishMqtt(String_sensor_htu.c_str(), HTU_MQTT_TOPIC);
-      htu21dDetected = true;
-    }
-
-    if (!htu21dDetected && !bmp280Detected && !noSensorDetected && currentMillis > 2000) {
-      Serial.println("No se detectó ningún sensor.");
-      noSensorDetected = true;
-    }
+  if (!htu21dDetected && !bmp280Detected && currentMillis > 2000) {
+    Serial.println("No se detectó ningún sensor.");
   }
 }
-
-
 String getLastItem() {
   
   int httpCode = http.GET();
@@ -121,7 +122,7 @@ String getLastItem() {
 
 
   String lastItemValue = "";
-  if (httpCode == 200) {
+  if (httpCode == HTTPCODE_SUCCESS) {
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, payload);
     int size = doc.size();
