38ab818bde1771d201bf947db9b0799f84d1a162
estebanolroberto <robertoestebanollivares@gmail.com>
2023-05-05 08:33:59 +0200

    Changes 5/5

--- ResAPI/myappTFG/app.js
+++ ResAPI/myappTFG/app.js
@@ -16,7 +16,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sensorTempRouter = require('./routes/sensorHTU');
 var sensorBMP280 = require('./routes/sensorBMP');
-var sensores = require('./routes/sensor');
+var devices = require('./routes/device');
 var app = express();
 
 // view engine setup
@@ -37,7 +37,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sensorHTU', sensorTempRouter);
 app.use('/sensorBMP', sensorBMP280);
-app.use('/sensores', sensores);
+app.use('/devices', devices);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
